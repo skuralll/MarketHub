64c577c163dd492ba3107a9c5aabf27bfc3cde02
skuralll <f20154@e.hiroshima-cu.ac.jp>
2024-03-25 13:35:43 +0900

    Support plugin reloading

--- src/main/kotlin/com/skuralll/markethub/MarketHub.kt
+++ src/main/kotlin/com/skuralll/markethub/MarketHub.kt
@@ -2,11 +2,17 @@ package com.skuralll.markethub
 
 import com.skuralll.markethub.command.CommandRegister
 import dev.jorel.commandapi.CommandAPI
+import dev.jorel.commandapi.CommandAPIBukkitConfig
 import dev.jorel.commandapi.kotlindsl.commandAPICommand
 import dev.jorel.commandapi.kotlindsl.playerExecutor
 import org.bukkit.plugin.java.JavaPlugin
 
 class MarketHub : JavaPlugin() {
+
+    override fun onLoad() {
+        CommandAPI.onLoad(CommandAPIBukkitConfig(this))
+    }
+
     override fun onEnable() {
         CommandAPI.onEnable()
         CommandRegister.registerCommands()
